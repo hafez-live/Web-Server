00454060bb25d4a81a18c36376bd769787add2e1
im-parsa <parsa.firoozi55@gmail.com>
2023-09-11 23:25:24 +0330

    :rocket: Fix(Core): configs

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppModule } from '../src/app.module';
 
